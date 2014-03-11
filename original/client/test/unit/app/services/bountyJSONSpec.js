42c21f9b0f2c8489946025e34e90311e9ab7259a
mehwww <mehwww7@gmail.com>
2014-03-11 21:40:27 +0800

    try json stringify

--- original/client/test/unit/app/services/bountyJSONSpec.js
+++ original/client/test/unit/app/services/bountyJSONSpec.js
@@ -6,12 +6,24 @@ describe('bountyJSON service', function () {
   }))
 
   it('should return proper object', function () {
-    expect(bountyJSON.parse('{_id: ObjectID("52d616d7cf7734b74670ec9d"),y:11}'))
-      .toEqual({_id:{$bountyType: 'ObjectID', $value: '52d616d7cf7734b74670ec9d'},y:11})
-    expect(bountyJSON.parse('{date:ISODate("1991-05-23T00:00:00Z")}'))
-      .toEqual({date:{$bountyType: 'ISODate', $value: '1991-05-23T00:00:00.000Z'}})
-//    expect(bountyJSON.parse('Date("1991-05-23T00:00:00Z")'))
-//      .toEqual({$bountyType: 'ISODate', $value: '1991-05-23T00:00:00.000Z'})
+    expect(bountyJSON.parse('{_id: ObjectID("52d616d7cf7734b74670ec9d"),x:1}'))
+      .toEqual({_id:{$bountyType: 'ObjectID', $value: '52d616d7cf7734b74670ec9d'},x:1})
+    expect(bountyJSON.parse('{date:ISODate("1991-05-23T00:00:00Z"),x:1}'))
+      .toEqual({date:{$bountyType: 'ISODate', $value: '1991-05-23T00:00:00.000Z'},x:1})
+    expect(bountyJSON.parse('{date:ISODate("1991-05-23T00:00:00Z"),x:1}'))
+      .toEqual({date:{$bountyType: 'ISODate', $value: '1991-05-23T00:00:00.000Z'},x:1})
+    expect(bountyJSON.parse('{regExp:/^\s*(new\s+)?(Date|RegExp)(\b)/}'))
+      .toEqual({regExp:{$bountyType: 'RegExp', $value: {$pattern:'^\s*(new\s+)?(Date|RegExp)(\b)',$flags:null}}})
+  })
+  it('should return proper string', function () {
+    console.log(bountyJSON.stringify({_id:{$bountyType: 'ObjectID', $value: '52d616d7cf7734b74670ec9d'},x:1}))
+
+//    expect(bountyJSON.stringify({_id:{$bountyType: 'ObjectID', $value: '52d616d7cf7734b74670ec9d'},x:1},'',false))
+//      .toEqual('{_id: ObjectID("52d616d7cf7734b74670ec9d"),x:1}')
+//    expect(bountyJSON.parse('{date:ISODate("1991-05-23T00:00:00Z"),x:1}'))
+//      .toEqual({date:{$bountyType: 'ISODate', $value: '1991-05-23T00:00:00.000Z'},x:1})
+//    expect(bountyJSON.parse('{date:ISODate("1991-05-23T00:00:00Z"),x:1}'))
+//      .toEqual({date:{$bountyType: 'ISODate', $value: '1991-05-23T00:00:00.000Z'},x:1})
   })
 //  it('should return proper string', function () {
 //    expect(bountyJSON.stringify({
